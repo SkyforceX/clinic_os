3ce3de47187b298b741ac1a408362d7f005295c1
SkyforceX <nhoaiphuc@gmail.com>
2026-04-21 12:49:04 +0700

    fix settings docx quality

--- apps/quality/utils.py
+++ apps/quality/utils.py
@@ -163,7 +163,7 @@ def convert_docx_to_pdf_with_libreoffice(docx_path: str, pdf_path: str):
     print(">>> RUN:", " ".join(cmd))
 
     # Nếu LibreOffice lỗi sẽ raise CalledProcessError
-    subprocess.run(cmd, condition=True)
+    subprocess.run(cmd, check=True)
 
     # Một số bản LibreOffice có thể đặt tên file hơi khác, nên chuẩn hoá lại
     if not os.path.exists(pdf_path):
